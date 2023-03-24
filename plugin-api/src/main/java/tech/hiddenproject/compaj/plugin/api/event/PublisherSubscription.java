19e93b83f4abdd16b50d313ca3cf6ef6c5a05358
danilarassohin <danilarassohin@gmail.com>
2023-03-25 01:04:53 +0300

    Changed plugin-api JDK version to 11

--- plugin-api/src/main/java/tech/hiddenproject/compaj/plugin/api/event/PublisherSubscription.java
+++ plugin-api/src/main/java/tech/hiddenproject/compaj/plugin/api/event/PublisherSubscription.java
@@ -1,12 +1,23 @@
 package tech.hiddenproject.compaj.plugin.api.event;
 
+import java.util.Objects;
+
 /**
  * Represents subscription on {@link EventPublisher}.
- *
- * @param topic Topic name
- * @param id Subscription id
  */
-public record PublisherSubscription(String topic, Long id) {
+public final class PublisherSubscription {
+
+  private final String topic;
+  private final Long id;
+
+  /**
+   * @param topic Topic name
+   * @param id    Subscription id
+   */
+  public PublisherSubscription(String topic, Long id) {
+    this.topic = topic;
+    this.id = id;
+  }
 
   @Override
   public boolean equals(Object obj) {
@@ -20,4 +31,25 @@ public record PublisherSubscription(String topic, Long id) {
     return topic().equals(publisherSubscription.topic())
         && id().equals(publisherSubscription.id());
   }
+
+  public String topic() {
+    return topic;
+  }
+
+  public Long id() {
+    return id;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(topic, id);
+  }
+
+  @Override
+  public String toString() {
+    return "PublisherSubscription[" +
+        "topic=" + topic + ", " +
+        "id=" + id + ']';
+  }
+
 }
